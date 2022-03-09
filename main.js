90472da37e1b78a340bc6cd646a26e794eb25694
Andry <94480311+andry359@users.noreply.github.com>
2022-03-09 23:31:34 +0300

    Добавил вывод информации с сервера

--- main.js
+++ main.js
@@ -7,10 +7,18 @@ const tabOneTitleTemperature = document.getElementById('scoreboardTitleNumber')
 const tabOneMainIcon = document.getElementById('mainIcon');
 const tabOneTitleCity = document.getElementById('titleCity');
 const tabOneAddCity = document.getElementById('addCity');
-// Блок ...
+// Блок tabs__body tab_02
+const tabTwoTitleCity = document.getElementById('tabTwoTitleCity');
+const tabTwoTemperature = document.querySelector('.Temperature');
+const tabTwoFeelsLike = document.querySelector('.Feels-like');
+const tabTwoWeather = document.querySelector('.Weather');
+const tabTwoSunrise = document.querySelector('.Sunrise');
+const tabTwoSunset = document.querySelector('.Sunset');
+// Блок tabs__body tab_03
 
-// Блок block__cities
-const blockCitiesListCity = document.getElementById('listCity');
+// Блок cities
+const citieslistCity = document.getElementById('listCity');
+const citiesAddedList = document.querySelectorAll('.added-cities');
 
 // ИСПОЛНИТЕЛИ:
 // Блок search
@@ -24,17 +32,8 @@ iconSearch.addEventListener('click', function () {
     searchInput.value;
     console.log(searchInput.value);
 });
-// Блок tabs__body tab_01
-searchInput.addEventListener('keyup', function (event) {
-    if (event.keyCode == 13) {
-        tabOneTitleCity.textContent = `${searchInput.value}`;
-    };
-});
-iconSearch.addEventListener('click', function () {
-    tabOneTitleCity.textContent = `${searchInput.value}`;
-});
 tabOneAddCity.addEventListener('click', function () {
-    blockCitiesListCity.insertAdjacentHTML('beforeend', `<li><a href="#">${searchInput.value}</a></li>`);
+    citieslistCity.insertAdjacentHTML('beforeend', `<li><a class="added-cities" href="#">${searchInput.value}</a></li>`);
 });
 
 searchInput.addEventListener('keyup', function () {
@@ -47,31 +46,66 @@ searchInput.addEventListener('keyup', function () {
         if (cityName) {
             let response = fetch(url);
             let json = response.then(result => result.json());
-            // let resultJson = JSON.parse(json);
-            // console.log(resultJson);
-            // json.main.temp
-            console.log(json.main.temp);
+            // Блок tab_1:
+            json.then(result => tabOneTitleCity.textContent = result.name);
+            json.then(result => tabOneTitleTemperature.textContent = `${(result.main.temp).toFixed(0)}°`);
+            // json.then(result => tabOneMainIcon.textContent =
+            //     function showNowTabInfo(params) {
+            //         const getUrlImg = (img) => `https://openweathermap.org/img/wn/${img}.png`
+            //         checkFavoriteCity(params.name);
+            //         tabOneMainIcon.src = getUrlImg(params.weather[0].icon);
+            //     }
+            // );
+            // let srcUrl = `http://openweathermap.org/img/w/${json.weather[0].icon}.png`;
+            // json.then(result => tabOneMainIcon.innerHTML = 
+            //     result.weather[0].icon);
+            //Блок tab_2:
+            json.then(result => tabTwoTitleCity.textContent = result.name);
+            json.then(result => tabTwoTemperature.textContent = `Temperature: ${(result.main.temp).toFixed(0)}°`);
+            json.then(result => tabTwoFeelsLike.textContent = `Feels like: ${(result.main.feels_like).toFixed(0)}°`);
+            json.then(result => tabTwoWeather.textContent = `Weather: ${result.weather[0].description}`);
+            //Нужно еще правильно распаковать Sunrise и Sunset!
+            json.then(result => tabTwoSunrise.textContent = `Sunrise: ${result.sys.sunrise}`);
+            json.then(result => tabTwoSunset.textContent = `Sunset: ${result.sys.sunset}`);
+
+            console.log(json);
         }
     };
 });
+iconSearch.addEventListener('click', function () {
+    let cityName = searchInput.value;
+    const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
+    const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
+    const url = `${serverUrl}?q=${cityName}&appid=${apiKey}&units=metric`;
+    console.log(url);
+    if (cityName) {
+        let response = fetch(url);
+        let json = response.then(result => result.json());
+        // Блок tab_1:
+        json.then(result => tabOneTitleCity.textContent = result.name);
+        json.then(result => tabOneTitleTemperature.textContent = `${(result.main.temp).toFixed(0)}°`);
+        // let srcUrl = `http://openweathermap.org/img/w/${json.weather[0].icon}.png`;
+        // json.then(result => tabOneMainIcon.innerHTML = 
+        //     result.weather[0].icon);
+        //Блок tab_2:
+        json.then(result => tabTwoTitleCity.textContent = result.name);
+        json.then(result => tabTwoTemperature.textContent = `Temperature: ${(result.main.temp).toFixed(0)}°`);
+        json.then(result => tabTwoFeelsLike.textContent = `Feels like: ${(result.main.feels_like).toFixed(0)}°`);
+        json.then(result => tabTwoWeather.textContent = `Weather: ${result.weather[0].description}`);
+        //Нужно еще правильно распаковать Sunrise и Sunset!
+        json.then(result => tabTwoSunrise.textContent = `Sunrise: ${result.sys.sunrise}`);
+        json.then(result => tabTwoSunset.textContent = `Sunset: ${result.sys.sunset}`);
 
-// function view() {
-//     let cityName = searchInput.value;
-//     const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
-//     const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
-//     const url = `${serverUrl}?q=${cityName}&appid=${apiKey}&units=metric`;
-//     console.log(url);
-// }
+        console.log(json);
 
-
-
-// Пока хз что это вообще
-// const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
-// const cityName = 'boston';
-// const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
-// const url = ${serverUrl}?q=${cityName}&appid=${apiKey};
-
-// const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
-// const url = `${serverUrl}?q=${cityName}&appid=${apiKey}`;
-
-// Блок block__cities
+        console.log(json);
+    }
+});
+// Можно доделать ещё опцию моментального вывода информации выбранного из избанного города, а не по ентору и картинке, как сейчас
+for (city of citiesAddedList) {
+    city.addEventListener('click', function (event) {
+        let nameCity = event.target.textContent;
+        searchInput.value = nameCity;
+        searchInput.click();
+    })
+}
---
+++ view.js
@@ -0,0 +1,14 @@
+const UIELEMENTS = {
+    // Блок search
+    searchInput: document.getElementById('searchInput'),
+    iconSearch: document.getElementById('imgSearch'),
+    // Блок tabs__body tab_01
+    tabOneTitleTemperature: document.getElementById('scoreboardTitleNumber'),
+    tabOneMainIcon: document.getElementById('mainIcon'),
+    tabOneTitleCity: document.getElementById('titleCity'),
+    tabOneAddCity: document.getElementById('addCity'),
+    // Блок ...
+
+    // Блок block__cities
+    blockCitiesListCity: document.getElementById('listCity'),
+}
